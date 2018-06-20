fdf42513f710a047e222936da204cb843a41986e
0x384c0 uID <0x384c0 uID>
2018-06-20 12:12:47 +0600

    fixed board state

--- tk/TKLogick.py
+++ tk/TKLogick.py
@@ -18,21 +18,22 @@ WIN_SCORE = (BOARD_SIZE * WIDTH)/HEIGHT - 1
 
 class Board():
 
-	__size = WIDTH * HEIGHT
-	__action_size = BOARD_SIZE
+	def __init__(self):
+		self.__size = WIDTH * HEIGHT
+		self.__action_size = BOARD_SIZE
 
-	__init_state = [INIT_BALLS_COUNT_IN_PIT] * BOARD_SIZE
-	__players_scores = {
-		1	:	0,	# player 1
-		-1	:	0	# player -1
-	}
-	__players_tuz = {
-		1	:	None,	# player 1
-		-1	:	None	# player -1
-	}
+		self.__init_state = [INIT_BALLS_COUNT_IN_PIT] * BOARD_SIZE
+		self.__players_scores = {
+			1	:	0,	# player 1
+			-1	:	0	# player -1
+		}
+		self.__players_tuz = {
+			1	:	None,	# player 1
+			-1	:	None	# player -1
+		}
+		self.pieces = data_array_to_one_hot(self.__init_state,MAX_BALLS_COUNT_IN_PIT)
 
 
-	pieces = data_array_to_one_hot(__init_state,MAX_BALLS_COUNT_IN_PIT)
 
 
 	def __getitem__(self, index): 
@@ -55,35 +56,49 @@ class Board():
 		last_pit_looped -= 1
 
 
-		# игрок берёт все камни из любой своей лунки «дом» и, начиная с этой же лунки, раскладывает их по одному против часовой стрелки в свои и чужие дома
-		game_state[move] = 0
-		for pit in range(move,last_pit):
-			if pit >= len(game_state):
-				pit = pit - len(game_state)
-			game_state[pit] += 1
+		if balls_in_first_pit == 1:
+			# Если в исходной лунке только один камень, то он перекладывается в следующую лунку.
+			last_pit_looped += 1
+			game_state[move] = 0
+			game_state[last_pit_looped] += balls_in_first_pit
+		else:
+			# игрок берёт все камни из любой своей лунки «дом» и, начиная с этой же лунки, раскладывает их по одному против часовой стрелки в свои и чужие дома
+			game_state[move] = 0
+			for pit in range(move,last_pit):
+				if pit >= len(game_state):
+					pit = pit - len(game_state)
+				game_state[pit] += 1
+
 
 		#Если последний коргоол попадает в дом соперника и 
 		#количество коргоолов в нём становится чётным, то коргоолы из этого дома переходят в казан игрока, совершившего ход.
 		if  (
 			self.__is_pit_dont_belongs_to_player(last_pit_looped,player) and
 			game_state[last_pit_looped] % 2 == 0
-			): # TODO:add checking is enemys pit 
+			):
 			self.__players_scores[player] += game_state[last_pit_looped]
 			game_state[last_pit_looped] = 0
+
+		opponents_last_pit = WIDTH - 1 if player == 1 else WIDTH / 2 - 1
+		if self.__players_tuz[-player] != None:
+			opponents_tuz = self.__players_tuz[-player] - WIDTH / 2 if  player == 1 else self.__players_tuz[-player] + WIDTH / 2
+		else:
+			opponents_tuz = None
 
 		#Если при ходе игрока А последний коргоол попадает в дом игрока Б и в нём после этого оказывается три коргоола, то этот дом объявляется тузом игрока А 
 		# 1) игрок не может завести себе туз в самом последнем (девятом) доме соперника,
 		# 2) игрок не может завести себе туз в доме с таким же порядковым номером, который имеет лунка-туз соперника,
 		# 3) каждый игрок в течение игры может завести себе только один туз.
-		if (game_state[last_pit_looped] == 3 and 
-			move < WIDTH - 1 and 
-			last_pit_looped != self.__players_tuz[-player] and 
-			self.__players_tuz[player] != None):
+		if (game_state[last_pit_looped] 	== 3					and 
+			move 							!= opponents_last_pit	and
+			last_pit_looped 				!= opponents_tuz		and
+			self.__players_tuz[player] 		== None):
 			#Эти три коргоола попадают в казан игрока
 			self.__players_scores[player] += game_state[last_pit_looped]
 			game_state[last_pit_looped] = 0
 			self.__players_tuz[player] = last_pit_looped
-		return data_array_to_one_hot(game_state,MAX_BALLS_COUNT_IN_PIT)
+		self.pieces = data_array_to_one_hot(game_state,MAX_BALLS_COUNT_IN_PIT)
+		return self.pieces
 
 	def __generate_valid_moves(self,player):
 		possible_moves = [0] * self.__action_size
@@ -96,24 +111,26 @@ class Board():
 				possible_moves[i] = 1 if game_state[i] > 0 else 0
 
 		if (self.__players_tuz[player] is not None):
-			possible_moves[i] = 1 if game_state[self.__players_tuz[player]] > 0  else 0
+			possible_moves[self.__players_tuz[player]] = 1 if game_state[self.__players_tuz[player]] > 0  else 0
 		return possible_moves
 
 	def __is_pit_dont_belongs_to_player(self,pit,player):
-		players_pit = list(range(0, self.__size / 2)) if player == 1 else list(range(self.__size / 2, self.__size))
+		if player == 1:
+			players_pit = list(range(0, int(self.__size / 2))) 
+		else:
+			players_pit = list(range(int(self.__size / 2), self.__size))
+		
 		if self.__players_tuz[-player] in players_pit:
 			players_pit.remove(self.__players_tuz[-player])
 		if self.__players_tuz[player] != None:
-			players_pit.add(self.__players_tuz[player])
+			players_pit.append(self.__players_tuz[player])
 		return pit not in players_pit
 
 
 
-
+#for tests
 	def get_player_score(self,player):
 		return self.__players_scores[player]
 
-
-
-
-
+	def set_player_score(self,score,player):
+		self.__players_scores[player] = score
--- tk/test/testTKLogick.py
+++ tk/test/testTKLogick.py
@@ -3,8 +3,9 @@ import unittest
 import sys
 sys.path.append('..')
 
-from ..TKLogick import Board
-from ..utils import *
+from TKLogick import Board
+from TKLogick import MAX_BALLS_COUNT_IN_PIT
+from utils import *
 
 class TestTKLogick(unittest.TestCase):
 	def setUp(self):
@@ -17,6 +18,15 @@ class TestTKLogick(unittest.TestCase):
 	def test_get_legal_moves(self):
 		self.assertEqual(self.board.get_legal_moves(1), 	[1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 		self.assertEqual(self.board.get_legal_moves(-1), 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+		
+		self.board.execute_move(1,1)
+		self.board.execute_move(1,1)
+		self.assertEqual(self.board.get_legal_moves(1),		[1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+		self.board.pieces = data_array_to_one_hot([9, 9, 9, 9, 9, 9, 5, 9, 9,		9, 2, 9, 9, 9, 9, 9, 9, 9],MAX_BALLS_COUNT_IN_PIT)
+		self.board.execute_move(6,1)
+		self.board.execute_move(7,1)
+		self.assertEqual(self.board.get_legal_moves(1), 	[1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0])
 
 	def test_has_legal_moves(self):
 		self.assertEqual(self.board.has_legal_moves(), 	True)
@@ -24,20 +34,45 @@ class TestTKLogick(unittest.TestCase):
 	def test_is_win(self):
 		self.assertEqual(self.board.is_win(1),		False)
 		self.assertEqual(self.board.is_win(-1),		False)
+		self.board.set_player_score(80,1)
+		self.board.execute_move(1,1)
+		self.assertEqual(self.board.is_win(1),		True)
 
 	def test_execute_move(self):
 		self.assertEqual(one_hot_batch_to_array(self.board.execute_move(1,1)),		[9, 1, 10, 10, 10, 10, 10, 10, 10,		0,  9, 9, 9, 9, 9, 9, 9, 9])
 		self.assertEqual(self.board.get_player_score(1),10)
 		self.assertEqual(self.board.get_player_score(-1),0)
+		self.setUp()
 		self.assertEqual(one_hot_batch_to_array(self.board.execute_move(1,-1)),		[9, 1, 10, 10, 10, 10, 10, 10, 10,		10, 9, 9, 9, 9, 9, 9, 9, 9])
-		self.assertEqual(self.board.get_player_score(1),10)
+		self.assertEqual(self.board.get_player_score(1),0)
 		self.assertEqual(self.board.get_player_score(-1),0)
+		self.setUp()
 		self.assertEqual(one_hot_batch_to_array(self.board.execute_move(15,1)),		[10, 10, 10, 10, 10, 10, 9, 9, 9,		9, 9, 9, 9, 9, 9, 1, 10, 10])
-		self.assertEqual(self.board.get_player_score(1),10)
+		self.assertEqual(self.board.get_player_score(1),0)
 		self.assertEqual(self.board.get_player_score(-1),0)
+		self.setUp()
 		self.assertEqual(one_hot_batch_to_array(self.board.execute_move(15,-1)),	[10, 10, 10, 10, 10, 0,  9, 9, 9,		9, 9, 9, 9, 9, 9, 1, 10, 10])
+		self.assertEqual(self.board.get_player_score(1),0)
+		self.assertEqual(self.board.get_player_score(-1),10)
+
+		self.assertEqual(one_hot_batch_to_array(self.board.execute_move(0,1)),		[1, 11, 11, 11, 11, 1,  10, 10, 10,		0, 9, 9, 9, 9, 9, 1, 10, 10])
 		self.assertEqual(self.board.get_player_score(1),10)
 		self.assertEqual(self.board.get_player_score(-1),10)
+
+	def test_tuz(self):
+		self.board.pieces = data_array_to_one_hot([9, 9, 9, 9, 9, 9, 9, 10, 10,		9, 9, 9, 9, 9, 9, 9, 1, 2],MAX_BALLS_COUNT_IN_PIT)
+		
+		# print(one_hot_batch_to_array(self.board.pieces))
+		self.assertEqual(one_hot_batch_to_array(self.board.execute_move(8,1)),		[9, 9, 9, 9, 9, 9, 9, 10, 1, 10, 10, 10, 10, 10, 10, 10, 2, 3])
+		self.assertEqual(self.board.get_legal_moves(1),								[1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+		
+		# print(one_hot_batch_to_array(self.board.pieces))
+		self.assertEqual(one_hot_batch_to_array(self.board.execute_move(7,1)),		[9, 9, 9, 9, 9, 9, 9, 1, 2,		11, 11, 11, 11, 11, 11, 11, 0, 3])
+		self.assertEqual(one_hot_batch_to_array(self.board.execute_move(9,1)),		[10, 10, 9, 9, 9, 9, 9, 1, 2,	1, 12, 12, 12, 12, 12, 12, 1, 4])
+		self.assertEqual(self.board.get_legal_moves(1),								[1, 1, 1, 1, 1, 1, 1, 1, 1,		0, 0, 0, 0, 0, 0, 0, 1, 0])
+
+
+
 
 
 if __name__ == '__main__':
